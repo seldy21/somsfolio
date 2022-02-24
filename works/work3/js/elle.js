165a43f73c2dc389014bb18f6651d65bf64fd1ef
seldy21 <seldy@naver.com>
2022-02-24 21:40:44 +0900

    favicon 수정3

--- works/work3/js/elle.js
+++ works/work3/js/elle.js
@@ -73,30 +73,33 @@ $('#article3').on('mouseleave',()=>{
 });
 
 //footer 흐르는 글자
+Zepto(function($) {
+  $(window).on('load', function() {
+    $.each($(".ezkorry-roller"), function(index, item) {
+      const wrapper = $("<div />", { class:"ezkorry-roller-wrapper"});
+      const roller = $(item);
+      roller.append(wrapper);
+      const span = roller.find('span').first();
+      wrapper.append(span);
+  
+      const span_width = span.get(0).offsetWidth;
+      const max_width = roller.width() + span_width;
+      let inner_width = span_width;
 
-$.each($(".ezkorry-roller"), function(index, item) {
-  const wrapper = $("<div />", { class:"ezkorry-roller-wrapper"});
-  const roller = $(item);
-  roller.append(wrapper);
-  const span = roller.find('h2').first();
-  wrapper.append(span);
-
-  const span_width = span.get(0).offsetWidth;
-  const max_width = roller.width() + span_width;
-  let inner_width = span_width;
-
-  while(max_width > inner_width) {
-   wrapper.append(span.clone());
-   inner_width += span_width;
-  }
-
- anime({
-   targets: wrapper.get(0),
-   translateX: {
-     value: '-=' + span_width + 'px',
-    duration: 3000
-  },
-  loop: true,
-  easing: 'linear'
-  });
+      while(max_width > inner_width) {
+        wrapper.append(span.clone());
+        inner_width += span_width;
+      }
+      
+      anime({
+        targets: wrapper.get(0),
+        translateX: {
+          value: '-=' + span_width + 'px',
+          duration: 5000
+        },
+        loop: true,
+        easing: 'linear'
+      });
+    });
+  })
 });
