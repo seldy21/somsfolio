6c148a3e16d5395310975e7e33fefbd7c6b6a382
seldy21 <seldy@naver.com>
2022-03-01 18:47:44 +0900

    work부분 추가

--- works/work1/js/hlb.js
+++ works/work1/js/hlb.js
@@ -1,6 +1,6 @@
 //vegas 적용
 //$("대상").method();
-$(".swiper").vegas({/*vegas.min.css, vegas.min.js 와 연결해야 함*/
+$(".main_images").vegas({/*vegas.min.css, vegas.min.js 와 연결해야 함*/
     delay:6000,
     slides: [
         { src: "images/mainImg.png"},/*이미지 경로 입력*/
@@ -13,3 +13,22 @@ $(".swiper").vegas({/*vegas.min.css, vegas.min.js 와 연결해야 함*/
 
 //aos적용
 AOS.init();
+
+//swiper 적용
+        
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3.5,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
